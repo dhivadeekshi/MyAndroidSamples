a314d8a828f209aec27d6015fea5283dca8ca826
Dhivakar Subramaniam <subramaniamdhivakar@gmail.com>
2018-05-11 11:08:15 +0530

    External Apps Samples
    Added new activiy for the External apps
    Updated the gradle plugin to 3.1.2

---
+++ app/src/main/java/com/dhivakar/mysamples/externalapps/ExternalAppsManager.java
@@ -0,0 +1,15 @@
+package com.dhivakar.mysamples.externalapps;
+
+import android.os.Bundle;
+
+import com.dhivakar.mysamples.BaseAppCompatActivity;
+import com.dhivakar.mysamples.R;
+
+public class ExternalAppsManager extends BaseAppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_orientations_manager);
+    }
+}
