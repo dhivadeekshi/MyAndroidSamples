7ddc299c2c358854c50668e054e56ec97862706d
Dhivakar Subramaniam <subramaniamdhivakar@gmail.com>
2018-06-14 22:55:04 +0530

    Deeplink
    Added deeplink using Activity scheme

---
+++ app/src/main/java/com/dhivakar/mysamples/activity/Deeplink.java
@@ -0,0 +1,16 @@
+package com.dhivakar.mysamples.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.dhivakar.mysamples.BaseAppCompatActivity;
+import com.dhivakar.mysamples.R;
+
+public class Deeplink extends BaseAppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_deeplink);
+    }
+}
